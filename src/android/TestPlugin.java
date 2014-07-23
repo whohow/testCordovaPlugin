d238939ca3cdfff7c82bd124c056208ec282d852
whohow <huhao.fun@gmail.com>
2014-07-23 12:03:50 -0700

    Update TestPlugin.java

--- src/android/TestPlugin.java
+++ src/android/TestPlugin.java
@@ -13,7 +13,7 @@ public class TestPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("testPlugin")) {
+        if (action.equals("echo")) {
             String message = args.getString(0);
             this.echo(message, callbackContext);
             return true;
