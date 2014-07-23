9fa6a92e203a8c2534ef15e71cfed9aa3daaddc4
whohow <huhao.fun@gmail.com>
2014-07-23 12:03:22 -0700

    Update testPlugin.js

--- www/testPlugin.js
+++ www/testPlugin.js
@@ -1,5 +1,5 @@
 window.echo = function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
-        }, "TestPlugin", "echo", [str]);
+        }, "testPlugin", "echo", [str]);
     };
